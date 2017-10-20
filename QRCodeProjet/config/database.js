0da5c0eec37c8e4333d056b3523de924c2bdaf1f
melninie <twybow91@gmail.com>
2017-10-20 15:45:54 +0200

    Initialisation du projet

---
+++ QRCodeProjet/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+		'port':'3306',
+        'user': 'root'
+    },
+	'database': 'qrcode',
+    'users_table': 'utilisateur'
+};
