d9ca10abbf0af78ad0306966941a6f1c763d12d5
melninie <twybow91@gmail.com>
2017-11-21 08:57:25 +0100

    Début partie etudiant

--- QRCodeProjet/app/Models/etudiantModel.js
+++ QRCodeProjet/app/Models/etudiantModel.js
@@ -2,23 +2,9 @@ var connection=require('../../config/dbconnection');
 
 var Etudiant={
 
-    /*ObtAllPromos:function(callback)
-    {
-        return connection.query("select * from promotion", callback);
-    },*/
-
-    ObtEtuId:function(id, callback)
-    {
-        return connection.query("select * from seance where idS=?", [id], callback);
-    }/*,
-
-    /!*PutUserId:function(id, nom, prenom, mail, promo, callback) {
-        return connection.query("update users SET nomU=?, prenomU=?, mailU=?, promotionU=? where id=?", [nom, prenom, mail, promo, id], callback)
-    },*!/
-
-    DelPromoId:function(id) {
-        return connection.query("delete from promotion where idP=?", [id])
-    }*/
+    ObtSeanceWithMatiereId:function(id, callback) {
+        return connection.query("select * from seance s LEFT JOIN matiere m ON s.matiereS=m.idM where idS = ?;", [id], callback)
+    },
 
 };
 module.exports=Etudiant;
--- QRCodeProjet/app/Routes/etudiantRoutes.js
+++ QRCodeProjet/app/Routes/etudiantRoutes.js
@@ -6,33 +6,31 @@ var router = require('express').Router();
 // ETUDIANT ==========================
 // =====================================
 
-router.get('/seance/:id?', function(req, res, next) {CheckLog(req, res, next, "ADMINISTRATION");},function(req, res) {
+router.get('/seance/:id?', function(req, res, next) {CheckLog(req, res, next, "ETUDIANT");},function(req, res) {
 
     if(req.params.id) {
-        var query = Etudiant.ObtEtuId(req.params.id, function(err,rows)
-        {
-            if(err)
-                console.log("Error Selecting : %s ",err );
-            if(rows.length <= 0) {
+        var query = Etudiant.ObtSeanceWithMatiereId(req.params.id, function(err,rows) {
+            if (err)
+                console.log("Error Selecting : %s ", err);
+            if (rows.length <= 0) {
                 res.render('errorRessource.ejs', {
                     page_title: "Error",
-                    ressource: "/etudiant/promotions/" + req.param("id")
+                    ressource: "/etudiant/seance/" + req.param("id")
                 });
             }
+
+            console.log(rows[0]);
+
+            //vérifications
+            //la bonne promo
+            if(req.user.promotionU != rows[0].promotionS){
+
+            }
+            //l'horaire
+
             res.render('signalerPresence.ejs',{page_title:"signalerPresence", seance:rows});
         });
     }
-    /*else {
-        var query = Etudiant.ObtAllPromos(function (err, rows) {
-            if (err)
-                console.log("Error Selecting : %s ", err);
-
-            res.render('signalerPresence.ejs', {page_title: "signalerPresence", promos: rows, chemin:"admin/promotions/"});
-        });
-    }
-
-    res.render('signalerPresence.ejs', {page_title: "signalerPresence", /!*promos: rows,*!/ chemin:"etudiant/seance/"});*/
-
 });
 
 module.exports = router;
