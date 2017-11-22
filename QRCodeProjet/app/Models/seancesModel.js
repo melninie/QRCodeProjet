561c1ed0ae5862ac88ec503a0ab0560710734126
Nicolas De Martino <nico.demartino@laposte.net>
2017-11-22 22:45:49 +0100

    Enseignant/seance : Generation QR_CODE + Gestion des présents par listing des etudiants de la seance

--- QRCodeProjet/app/Models/seancesModel.js
+++ QRCodeProjet/app/Models/seancesModel.js
@@ -10,8 +10,25 @@ var Seance={
         return connection.query("select * from seance where idS=?", [id], callback)
     },
 
-    ObtSeanceEnseignant:function(callback){
-        return connection.query("", callback)
+    ObtSeanceEnseignant:function(idEnseignant, callback){
+        return connection.query("SELECT s.idS, s.nomS, s.dateS, s.heureDebut, s.heureFin, s.valideS, "
+            + "m.idM, m.nomM, "
+            + "p.idP, p.nomP "
+            + "from seance s "
+            + "inner join matiere m on s.matiereS=m.idM "
+            + "inner join promotion p on m.promotionS=p.idP "
+            + "where s.userS=? and s.valideS=0 Order By s.dateS, s.heureFin LIMIT 1", [idEnseignant], callback);
+    },
+    ObtEtudiantEnseignant:function(idSeance, callback){
+        return connection.query("SELECT u.id, u.nomU, u.prenomU from seance s "
+            + "inner join matiere m on s.matiereS=m.idM "
+            + "inner join promotion p on m.promotionS=p.idP "
+            + "inner join users u on p.idP=u.promotionU "
+            + "where s.idS=? Order By u.nomU, u.prenomU", [idSeance], callback);
+    },
+
+    ObtBadgeEtuSeance:function(idEtudiant, idSeance, callback){
+        return connection.query("SELECT * from badge where utilisateurB=? and seanceB=?", [idEtudiant, idSeance], callback);
     },
 
     CheckSeance: function(nom, date, hDebut, hFin, matiere, enseignant, callback){
@@ -25,6 +42,11 @@ var Seance={
     PutSeanceId:function(id, nom, date, hDebut, hFin, commentaire, matiere, user, callback) {
         return connection.query("update seance SET nomS=?, dateS=?, heureDebut=?, heureFin=?, commentaire=?, matiereS=?, userS=? where idS=?", [nom, date, hDebut, hFin, commentaire, matiere, user, id], callback)
     },
+
+    ValiderSeance:function(id, commentaire, callback) {
+        return connection.query("update seance SET commentaire=?, valideS=1 where idS=?", [commentaire, id], callback)
+    },
+
 
     DelSeanceId:function(id) {
         return connection.query("delete from seance where idS=?", [id])
--- QRCodeProjet/app/Routes/enseignantRoutes.js
+++ QRCodeProjet/app/Routes/enseignantRoutes.js
@@ -1,26 +1,76 @@
 var Seance = require('../Models/seancesModel');
 var CheckLog = require('../CheckLogin');
+
 var router = require('express').Router();
+var moment = require('moment');
 
 // =====================================
 // ETUDIANT ==========================
 // =====================================
 
-router.get('/seance', function(req, res, next) {CheckLog(req, res, next, "ENSEIGNANT");},function(req, res) {
+router.get('/seance', function(req, res, next) {CheckLog(req, res, next, "ENSEIGNANT");},function(req, res)
+{
+    var query = Seance.ObtSeanceEnseignant(req.user.id, function(err,rows)
+    {
+        if(err)
+            console.log("Error Selecting : %s ",err );
+        if(rows.length <= 0)
+            res.render('errorRessource.ejs', { page_title: "Error", ressource: "/enseignant/seance" });
 
-        var query = User.ObtAllEtudiant(req.params.id, function(err,rows)
+        rows.forEach(function (element) {
+            element.dateS = element.dateS.getUTCFullYear()+"-"+(element.dateS.getUTCMonth()+1)+"-"+element.dateS.getDate();
+        });
+
+
+        var query2 = Seance.ObtEtudiantEnseignant(rows[0].idS, function(err,rows2)
         {
             if(err)
                 console.log("Error Selecting : %s ",err );
-            if(rows.length <= 0) {
-                res.render('errorRessource.ejs', {
-                    page_title: "Error",
-                    ressource: "/enseignant/seance" + req.param("id")
+            if(rows2.length <= 0)
+                res.render('errorRessource.ejs', { page_title: "Error", ressource: "/enseignant/seance" });
+
+            var rowsbadge = [];
+
+            rows2.forEach(function (element)
+            {
+                var query3 = Seance.ObtBadgeEtuSeance(element.id, rows[0].idS, function (err, rows3)
+                {
+                    if (err)
+                        console.log("Error Selecting : %s ", err);
+                    if(rows3.length==0)
+                        rowsbadge.push(0);
+                    else
+                        rowsbadge.push(1);
+
+                    console.log(rows3);
+
+                    if(rowsbadge.length == rows2.length)
+                    {
+                        res.render('validerPresence.ejs', { page_title: "validerPresence", seance: rows,
+                            etudiants: rows2, badge: rowsbadge, chemin:"enseignant/seance/" });
+                    }
                 });
-            }
-            res.render('validerPresence.ejs',{page_title:"validerPresence", seance:rows});
+            });
         });
+    });
 });
+
+router.put('/seance/:id?', function(req, res, next){ CheckLog(req, res, next, "ENSEIGNANT");}, function(req, res)
+{
+    if (req.param("id"))
+    {
+        console.log("PUT SEANCE")
+        var commentaire = req.body.commentaire;
+        var id = req.param("id");
+
+        var query = Seance.ValiderSeance(id, commentaire, function (err, rows) {
+            if (err)
+                console.log("Error Selecting : %s ", err);
+        });
+    }
+});
+
+
 
 module.exports = router;
 
