88328befa729b54490e65115690ffa5ae66b5dba
seb9122 <seb.verneyre@hotmail.fr>
2018-03-12 21:55:30 +0100

    Url pour trombi + ajustement liens admin

--- QRCodeProjet/assets/scripts/script.js
+++ QRCodeProjet/assets/scripts/script.js
@@ -72,16 +72,16 @@ function confirmBeforeDeleteAdmin(idItem, chemin) {
 
 function deleteItem(idItem, chemin) {
     $.ajax({
-        //url: "http://qrcode.guillaumeperes.fr/"+chemin+idItem,
-        url:"http://localhost:8080/"+chemin+idItem,
+        url: "http://qrcode.guillaumeperes.fr/"+chemin+idItem,
+        //url:"http://localhost:8080/"+chemin+idItem,
         type: 'delete',
     });
 }
 
 function modifyItem(idItem, chemin) {
     $.ajax({
-        //url: "http://qrcode.guillaumeperes.fr/"+chemin+"/"+idItem,
-        url:"http://localhost:8080/"+chemin+idItem,
+        url: "http://qrcode.guillaumeperes.fr/"+chemin+"/"+idItem,
+        //url:"http://localhost:8080/"+chemin+idItem,
         data: $("#form").serialize(),
         type: 'put',
     });
